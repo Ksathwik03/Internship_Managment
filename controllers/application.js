de574872361a0c38d68cde9fbefa607353106f9b
VineethKumarM <munigyala@gmail.com>
2022-05-10 17:10:22 +0530

    admin post

--- controllers/application.js
+++ controllers/application.js
@@ -1,223 +1,192 @@
-const { default: mongoose } = require("mongoose")
-const Application = require("../models/Application")
-const Faculty = require("../models/faculty")
-const Internship = require("../models/internship")
-const Student = require("../models/student")
+const { default: mongoose } = require("mongoose");
+const Application = require("../models/Application");
+const Faculty = require("../models/faculty");
+const Internship = require("../models/internship");
+const Student = require("../models/student");
 
-exports.ApplyForIntern = async(req,res) => {
-    
-    try{
-        let studentId = req.user.id
-        let intershipId = req.params.internshipId
-        let {experience , whyHire} = req.body
-        let facultyId = await Internship.findById(intershipId).facultyId
+exports.ApplyForIntern = async (req, res) => {
+	try {
+		let studentId = req.user.id;
+		let intershipId = req.params.internshipId;
+		let { experience, whyHire } = req.body;
+		let facultyId = await Internship.findById(intershipId).facultyId;
 
-        const newApplication = new Application({
-                                facultyId: facultyId,
-                                intershipId : intershipId,
-                                experience: experience,
-                                whyHire: whyHire,
-                                studentId: studentId                    
-                            })
-        
-        newApplication.save()
-        
-        return res.status(200).json({
-            success: true,
-            data: newApplication,
-        });
+		const newApplication = new Application({
+			facultyId: facultyId,
+			intershipId: intershipId,
+			experience: experience,
+			whyHire: whyHire,
+			studentId: studentId,
+		});
 
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
+		newApplication.save();
 
-}
+		return res.status(200).json({
+			success: true,
+			data: newApplication,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
-exports.appliedInterns = async(req,res) => {
-    
-    try{
-        let {applicationId} = req.params.applicationId
-        
-        const applied = await Application.find({'intershipId': applicationId})
-        
-        return res.status(200).json({
-            success: true,
-            data: applied,
-        });
+exports.appliedInterns = async (req, res) => {
+	try {
+		let { applicationId } = req.params.applicationId;
 
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
+		const applied = await Application.find({ intershipId: applicationId });
 
+		return res.status(200).json({
+			success: true,
+			data: applied,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
-exports.appliedInternships = async(req,res) => {
-    
-    try{
-        let {studentId} = req.user.id
-        const applied = await Application.find({'studentId': studentId})
-        
-        return res.status(200).json({
-            success: true,
-            data: applied,
-        });
+exports.appliedInternships = async (req, res) => {
+	try {
+		let { studentId } = req.user.id;
+		const applied = await Application.find({ studentId: studentId });
 
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
+		return res.status(200).json({
+			success: true,
+			data: applied,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
+exports.approveIntern = async (req, res) => {
+	try {
+		const applicationId = req.params.applicationId;
+		const application = await Application.findOneAndUpdate(
+			{ id: applicationId },
+			{ isApproved: true }
+		);
 
-exports.approveIntern = async(req,res) => {
-    
-    try{
-        
-        const applicationId = req.params.applicationId
-        const application = await Application.findOneAndUpdate(
-                                                {'id': applicationId},
-                                                {'isApproved' : true})
+		return res.status(200).json({
+			success: true,
+			data: application,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
-        
-        return res.status(200).json({
-            success: true,
-            data: application,
-        });
+exports.detailedApplication = async (req, res) => {
+	try {
+		const applicationId = req.params.applicationId;
+		const application = await Application.findOne({ id: applicationId });
+		const student = Student.findOne({ id: application.studentId });
 
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
+		return res.status(200).json({
+			success: true,
+			data: application,
+			student: student,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
+exports.facultyInterships = async (req, res) => {
+	try {
+		const facultyId = req.user.id;
+		const application = await Application.find({ id: facultyId });
 
-exports.detailedApplication = async(req,res) => {
-    
-    try{    
-        const applicationId = req.params.applicationId
-        const application = await Application.findOne({'id': applicationId})
-        const student= Student.findOne({'id' : application.studentId})
-        
-        return res.status(200).json({
-            success: true,
-            data: application,
-            student: student
-        });
-    }
+		return res.status(200).json({
+			success: true,
+			data: application,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
+exports.postInternship = async (req, res) => {
+	try {
+		const { email, stipend, minCGPA, description } = req.body.values;
 
-exports.facultyInterships = async(req,res) => {
-  
-    try{    
-        const facultyId = req.user.id
-        const application = await Application.find({'id': facultyId})
-        
-        return res.status(200).json({
-            success: true,
-            data: application,
-        });
-    }
+		const faculty = await Faculty.findOne({ email: email });
+		let facultyId;
+		if (faculty) {
+			facultyId = faculty._id;
+		} else {
+			return res.status(500).json({
+				success: false,
+				error: `incorrect email`,
+			});
+		}
 
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
+		const internship = new Internship({
+			facultyId: facultyId,
+			stipend: stipend,
+			minCGPA: minCGPA,
+			description: description,
+		});
+		await internship.save();
+		return res.status(200).json({
+			success: true,
+			data: internship,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
-exports.postInternship = async(req,res) => {
-    
-    try{
-        const {email , stipend , minCGPA , description} = req.body
-        const faculty = await Faculty.findOne({'email': email})
-        let facultyId
-        if(faculty){
-            facultyId = faculty._id
-        }
-        else{
-            return res.status(500).json({
-                success: false,
-                error: `incorrect email`
-            })
-        }
-        
-        const internship = new Internship({
-                             facultyId: facultyId,
-                             stipend:stipend,
-                             minCGPA:minCGPA,
-                             description: description   
-                            })
-        await internship.save()
-        return res.status(200).json({
-            success: true,
-            data: internship,
-        });
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
+exports.getAllAvaliableInternships = async (req, res) => {
+	try {
+		const internships = await Internship.find({});
+		return res.status(200).json({
+			success: true,
+			data: internships,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
 
-}
-
-
-exports.getAllAvaliableInternships = async(req,res) => {
-    
-    try{
-        const internships = await Internship.find({})
-        return res.status(200).json({
-            success: true,
-            data: internships,
-        });
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
-
-exports.getDetailedIntershipDetails = async(req,res) => {
-    
-    try{
-        const internshipId = req.params.internshipId
-        const internship = await Internship.findOne({'_id': internshipId})
-        const faculty = await Faculty.findOne({'_id': internship.facultyId})
-        return res.status(200).json({
-            success: true,
-            data: internship,
-            faculty: faculty
-        });
-    }
-    catch(err){
-        return res.status(500).json({
-            success: false,
-            error: `Error occured user ${err}`
-        })
-    }
-}
+exports.getDetailedIntershipDetails = async (req, res) => {
+	try {
+		const internshipId = req.params.internshipId;
+		const internship = await Internship.findOne({ _id: internshipId });
+		const faculty = await Faculty.findOne({ _id: internship.facultyId });
+		return res.status(200).json({
+			success: true,
+			data: internship,
+			faculty: faculty,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: `Error occured user ${err}`,
+		});
+	}
+};
